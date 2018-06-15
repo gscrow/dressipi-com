ccdec63035e331406b69b64466c7b48f270453d7
hassanisk <hiskaf1@gmail.com>
2018-06-15 11:13:52 +0100

    Add sticky and scrolling delays

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,67 @@
 $(document).ready(function() {
-
   // smooth scrolling
-  $('.anchor-link').on('click', function() {
+  $(".anchor-link").on("click", function() {
     var scrollTop = $(this.hash).offset().top - 30;
 
-    $('html,body').animate({
-      scrollTop: scrollTop
-    }, 1000);
+    $("html,body").animate(
+      {
+        scrollTop: scrollTop
+      },
+      1000
+    );
     return false;
   });
 
-
-  $('.anchor-link-privacy').on('click', function() {
+  $(".anchor-link-privacy").on("click", function() {
     var scrollTop = $(this.hash).offset().top - 155;
 
-    $('html,body').animate({
-      scrollTop: scrollTop
-    }, 1000);
+    $("html,body").animate(
+      {
+        scrollTop: scrollTop
+      },
+      1000
+    );
+    return false;
+  });
+
+  $(".side-nav-item").on("click", function() {
+    var scrollTop = $(this.hash).offset().top - 130;
+
+    $("html,body").animate(
+      {
+        scrollTop: scrollTop
+      },
+      1000
+    );
     return false;
   });
 
   // mobile menu trigger
-  $('.header-mobile-button').on('click', function() {
-    $(this).toggleClass('header-mobile-button--open');
-    $('.navigation').toggleClass('navigation--open');
+  $(".header-mobile-button").on("click", function() {
+    $(this).toggleClass("header-mobile-button--open");
+    $(".navigation").toggleClass("navigation--open");
   });
 
   // affix header
-  $('.header').affix({
-    offset: {
-      top: 1000
-    }
-  }).on('affix-top.bs.affix', function() {
-    $(this).addClass('hold').delay(400).queue(function() {
-      $(this).removeClass('hold').dequeue();
+  $(".header")
+    .affix({
+      offset: {
+        top: 1000
+      }
+    })
+    .on("affix-top.bs.affix", function() {
+      $(this)
+        .addClass("hold")
+        .delay(400)
+        .queue(function() {
+          $(this)
+            .removeClass("hold")
+            .dequeue();
+        });
     });
+
+  //sticky side nav
+  var sticky = new Waypoint.Sticky({
+    element: $(".sticky-nav")[0]
   });
 });
